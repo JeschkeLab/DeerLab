f8ddfb8305780ddaad88061e3abbce035567733e
Luis Fabregas <luisf@student.ethz.ch>
2019-06-26 16:05:16 +0200

    Background fit start finder added as well as old version of APT kernel constructor

---
+++ getBackgroundStart.m
@@ -0,0 +1,70 @@
+function [FitStartTime,FitStartPos]=getBackgroundStart(Signal,TimeAxis,EndCutoffPos,BckgModel,ModelParam)
+
+if nargin<5
+  BckgModel = 'exponential';
+  ModelParam = [];
+end
+if nargin<4
+  EndCutoffPos = length(TimeAxis);
+end
+
+%Get zero-time position
+[~,ZeroTimePosition] = min(abs(TimeAxis));
+TimeAxis = TimeAxis(ZeroTimePosition:EndCutoffPos);
+Signal = real(Signal((ZeroTimePosition:EndCutoffPos)));
+Length = length(TimeAxis);
+
+[Kernel,NormConstant,~,APT_TimeAxis,Crosstalk] = getAPTkernel(Length);
+
+% imagflag=get(handles.imaginary,'Value');
+% dcmplx=imagflag*handles.cmplx; % Complex data?
+%
+% % reference deconvolution for variable-time DEER, normalization for
+% % constant-time DEER
+% if handles.ctvt,
+%     z=handles.A_vexp;
+%     ref=Signal(1,:);
+%     sc=max(real(ref));
+%     sig=Signal(2,:);
+%     Signal=real(sig)./real(ref)+1i*imag(ref)/sc;
+% else
+% 	Signal=Signal/max(real(Signal));
+% end;
+
+% Adaptive background correction
+%Search for background fit start between 10% and 60% of the signal
+StartPosMin = round(0.1*Length);
+if StartPosMin<1
+  StartPosMin=1;
+end;
+StartPosMax = round(0.6*Length);
+if StartPosMax<5
+  StartPosMax=5;
+end;
+
+Merit = zeros(1,StartPosMax-StartPosMin);
+
+for FitStartPos=StartPosMin:StartPosMax
+  
+  FitTimeAxis=TimeAxis(FitStartPos:length(TimeAxis)); % time window of baseline region
+  FitData=Signal(FitStartPos:length(Signal)); % experimental data in this window
+  
+  % Background fit
+  Background=fitBackground(FitData,TimeAxis,FitTimeAxis,BckgModel,ModelParam);
+  
+  FormFactor=Signal-Background; % subtract background
+  FormFactor=FormFactor./Background; % divide by background, eqn [13]
+  
+  FormFactor = FormFactor/max(FormFactor); % normalize
+  [FreqDimension,~] = size(Kernel); % size of kernel
+  FreqDistribution=zeros(1,FreqDimension); % initialize distribution
+  for k=1:FreqDimension % sum in eqn [21]
+    FreqDistribution(k)=FreqDistribution(k)+sum(Kernel(k,:).*FormFactor.*APT_TimeAxis)/NormConstant(k);
+  end;
+  APTdistribution = Crosstalk\FreqDistribution'; % crosstalk correction, eqn [22]
+  Merit(FitStartPos - StartPosMin + 1) = sum(abs(APTdistribution(1:3)));
+end;
+
+[~,OptStartPos] = min(Merit);
+FitStartPos = OptStartPos + StartPosMin - 1;
+FitStartTime = TimeAxis(FitStartPos);
---
+++ tests/getBackgroundStart_integer.m
@@ -0,0 +1,31 @@
+function [err,data] = test(opt,olddata)
+
+%==============================================================
+% Get start of background fit ensure that integer is returned
+%==============================================================
+
+%Parameters
+Offset = 2;
+ModulationDepth = 0.35;
+DecayRate = 0.0005;
+Length = 200;
+TimeStep = 16;
+TimeAxis = linspace(TimeStep,Length*TimeStep,Length) - TimeStep;
+%Construct some dipolar evolution function from Fresnel integral
+dipevo = 1-2*fresnels(TimeAxis*2*pi*1/(15^3));
+%Construct background
+bckg = exp(-DecayRate*TimeAxis);
+%Account modulation depth for the offset=1
+adaptedmodulationDepth = ModulationDepth*(1+Offset);
+FormFactor = (1 - adaptedmodulationDepth) + adaptedmodulationDepth*dipevo;
+FormFactor = FormFactor + Offset;
+Signal = FormFactor.*bckg;
+
+
+FitStartTime = getBackgroundStart(Signal,TimeAxis);
+
+%Check for errors
+err = mod(FitStartTime,1)>0;
+data = [];
+
+end
