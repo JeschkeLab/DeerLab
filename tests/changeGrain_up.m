22add0d2adecc54ed50681c73ff1bd20f331c9ec
Luis Fabregas <luisf@student.ethz.ch>
2019-06-27 08:55:22 +0200

    Added changeGrain function for up/down interpolation of traces (now all old pre-processing functions have been reimplemented)

---
+++ tests/changeGrain_up.m
@@ -0,0 +1,25 @@
+function [err,data] = test(opt,olddata)
+
+%======================================================
+% Grain up function test
+%======================================================
+
+TimeStep = 2;
+Grain = TimeStep:TimeStep:100;
+originalData = 2 - log10(Grain);
+TimeStepUp = 8;
+GrainUp = TimeStepUp:TimeStepUp:100;
+grainUpData = 2 - log10(GrainUp);
+
+
+
+[grainedOutput,grainedTimeAxis] = changeGrain(originalData,Grain',TimeStepUp);
+
+
+err(1) = any(abs(grainedOutput - grainUpData)>1e-10);
+err(2) = any(abs(grainedTimeAxis - GrainUp)>1e-10);
+
+err = any(err);
+data = [];
+
+end
