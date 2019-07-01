a835d84ae894d14204106234857cca9eae301203
Luis Fabregas <luisf@student.ethz.ch>
2019-07-01 10:04:40 -0700

    daopts: test added to check that options are passed right to prepareSignal

--- daopts.m
+++ daopts.m
@@ -2,8 +2,13 @@ classdef daopts
     
     properties
         BackgroundModel = 'exponential'
+        ModelParam = [];
         FitStartSearch = 'auto'
         FitStartPos = [];
+        EndCutoffPos = [];
+        PhaseCorrection = 'auto';
+        Phase = [];
+        ZeroTime = [];
     end
     
     properties (SetAccess = private)
@@ -19,7 +24,7 @@ classdef daopts
         function obj = daopts(varargin)
             if nargin>0
                 if mod(nargin,2)
-                    error('Error \n Wrong number of arguments')
+                    error('Wrong number of arguments')
                 end
                 for i=1:2:length(varargin)
                     obj = setProperty(obj,varargin{i},varargin{i+1});
@@ -34,7 +39,7 @@ classdef daopts
             if any(strcmp(allowedInput,string)) && ~isa(string,'numerical')
                 obj.BackgroundModel = string;
             else
-                error('daopts:incorrectType','Error \n ''%s'' is not a valid input of the BackgroundModel property.',string)
+                error('daopts:incorrectType','''%s'' is not a valid input of the BackgroundModel property.',string)
             end
         end
         
@@ -48,13 +53,66 @@ classdef daopts
                     obj.FitStartPos = 1;
                 end
             else
-                error('daopts:incorrectType','Error \n ''%s'' is not a valid input of the BackgroundModel property.',string)
+                error('daopts:incorrectType','''%s'' is not a valid input of the FitStartSearch property.',string)
+            end
+        end
+        
+        function obj = set.ModelParam(obj,value)
+            if ~isa(value,'numeric')
+                error('daopts:incorrectType','Error \n ''%s'' is not a valid input of the ModelParam property.',value)
+            end
+            switch obj.BackgroundModel
+                
+                case {'exponential','polyexp','fractal'}
+                    if ~isempty(value)
+                        warning('ModelParam property is not required for the ''%s'' background model',obj.BackgroundModel)
+                    end
+                case 'polynomial'
+                    if isempty(value)
+                        error('ModelParam property is empty but required for the ''polynomial'' background model')
+                    else
+                        if value<0
+                            error(' Negative values for the ModelParam property are not be adopted.')
+                        end
+                        if mod(value,1)~=0
+                            error(' ModelParam property must be set to an integer number.')
+                        else
+                            obj.ModelParam = value;
+                        end
+                    end
+            end
+        end
+        
+        function obj = set.PhaseCorrection(obj,string)
+            allowedInput = {'auto','manual'};
+            if any(strcmp(allowedInput,string)) && ~isa(string,'numerical')
+                obj.BackgroundModel = string;
+                if strcmp(string,'auto')
+                    obj.Phase = [];
+                end
+            else
+                error('daopts:incorrectType','''%s'' is not a valid input of the PhaseCorrection property.',string)
+            end
+        end
+
+        function obj = set.Phase(obj,value)
+            if ~isa(value,'numeric')
+                error('daopts:incorrectType','Error \n ''%s'' is not a valid input of the Phase property.',value)
+            end
+            if strcmp(obj.PhaseCorrection,'auto')
+                warning('PhaseCorrection property is set to ''auto''. The Phase property is not required and will not be set.')
+            else
+                if value<0
+                    error(' Negative values for the Phase property are not permited.')
+                end
+                obj.Phase = value;
             end
         end
         
     end
     
     methods(Access = private)
+        
         function obj = setProperty(obj,Property,Value)
             if ~isa(Property,'char') && ~isa(Property,'string')
                 error('daopts:incorrectType','Error \n Invalid first data argument')
---
+++ tests/prepareSignal_options.m
@@ -0,0 +1,34 @@
+function [err,data] = test(opt,olddata)
+
+%======================================================
+% Test data preparation function
+%======================================================
+
+%Parameters
+Dimension = 200;
+TimeStep = 0.008;
+rmin = (4*TimeStep*52.04/0.85)^(1/3);
+rmax = 6*(Dimension*TimeStep/2)^(1/3);
+TimeAxis = linspace(0,TimeStep*Dimension,Dimension);
+Length = length(TimeAxis);
+
+DistanceAxis = linspace(rmin,rmax,Dimension);
+Distribution = gaussian(DistanceAxis,3,0.5);
+Distribution = Distribution'/sum(Distribution);
+Background = exp(-0.05*TimeAxis);
+Kernel = getKernel(Dimension,TimeStep/1000);
+
+DipEvoFcn = Kernel*Distribution;
+DipEvoFcn = DipEvoFcn';
+ExpData = (DipEvoFcn + 10).*Background;
+
+options = daopts('BackgroundModel','polyexp');
+
+Signal = pdsdata('TimeAxis',TimeAxis,'ExpData',ExpData);
+Signal = Signal.prepare(options);
+
+err = any(abs(Signal.DipEvoFcn - DipEvoFcn)>1e-2);
+
+data = [];
+
+end
