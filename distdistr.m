46a1f0452817cd7a3ff4ee7dce888cb1124db89d
Luis Fabregas <luisf@student.ethz.ch>
2019-07-02 10:51:16 -0700

    distdistr: added public fcn for getting current fitted signal and added better control over input distribution

--- distdistr.m
+++ distdistr.m
@@ -4,12 +4,13 @@ classdef distdistr
       Distribution
       DistanceAxis
       ExpSignal
-      FitSignal
       TimeAxis
    end
    
-   properties (Access = private)
+   properties (SetAccess = private)
       Method
+      FitSignal
+      Length
    end
    
    properties (Hidden)
@@ -18,25 +19,43 @@ classdef distdistr
     
    methods
         function obj = distdistr(varargin)
+            %Check if user given inputs
             if nargin>0
+                %Only pairwise inputs accepted
                 if mod(nargin,2)
                     error('Wrong number of arguments')
                 end
+                %Set properties given to constructor
                 for i=1:2:length(varargin)
                     obj = setProperty(obj,varargin{i},varargin{i+1});
                 end
             end
+            %Give unique ID to the variable
             JavaID =  java.util.UUID.randomUUID;
             obj.ID = JavaID.toString;
         end 
         
         function obj = set.Distribution(obj,Distribution)
-            CallStack = dbstack(1);
-            obj.Method = CallStack(1).name;
+            %Get function call stack
+            CallStack = dbstack;
+            %Look for function calling the class functions
+            i = 1;
+            while ~isempty(strfind(CallStack(i).name,'distdistr'))
+            i  = i + 1;
+            end
+            %Store which function/method constructed the distribution
+            obj.Method = CallStack(i).name;
+            %Check distribution array
+            if ~iscolumn(Distribution)
+               Distribution = Distribution'; 
+            end
+            obj.Length = length(Distribution);
+            %Force integral normalization
+            Distribution = Distribution/sum(Distribution);
             obj.Distribution = Distribution;
         end
         
-    function plotDistr(obj)
+    function plot(obj,CompDistAxis,CompDistribution)
       if ~isempty(obj.Distribution)
         Figure = findobj('Tag',sprintf('ID: %s',obj.ID));
         if isempty(Figure)
@@ -46,27 +65,37 @@ classdef distdistr
           figure(Figure);
           clf(Figure);
         end
+        %Plot distribution
         subplot(121)
-        plot(obj.DistanceAxis,obj.Distribution,'k','LineWidth',2)
-        axis tight
-        xlabel('Distance [nm]')
-        ylabel('P(r)')
+        plot(obj.DistanceAxis,obj.Distribution,'r','LineWidth',2)
+        %If requested plot against input distribution
+        if nargin>1
+        hold on
+        plot(CompDistAxis,CompDistribution,'k','LineWidth',2)
+        end
+        axis tight, box on, grid on
+        xlabel('Distance [nm]'),ylabel('P(r)')
+        %Plot 
         subplot(122)
         hold on
         plot(obj.TimeAxis,obj.ExpSignal,'k','LineWidth',2)
         plot(obj.TimeAxis,obj.FitSignal,'r','LineWidth',2)
-        axis tight
-        box on, grid on
-        xlabel('Time [ns]')
-        ylabel('Amplitude')
+        axis tight, box on, grid on
+        xlabel('Time [ns]'),ylabel('Amplitude')
         legend('Input','Fit')
       else
         error('Data has not been prepared.')
       end
     end
 
-        
-        
+    function obj = getFitSignal(obj)
+        TimeStep = obj.TimeAxis(2) - obj.TimeAxis(1);
+        Kernel = getKernel(obj.Length,TimeStep);
+        obj.FitSignal = Kernel*obj.Distribution;
+    end
+    
+    
+    
    end
    
        
