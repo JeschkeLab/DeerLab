215745ae1016d27971c2ba8a065eed848d8780c1
Luis Fabregas <luisf@student.ethz.ch>
2019-07-10 10:14:35 -0700

    gaussian: Exact definition of the Gaussian function now is correctly implemented

--- bin/gaussian.m
+++ bin/gaussian.m
@@ -1,3 +1,11 @@
-function y = gaussian(x,x0,w)
-    y = exp(-(x  - x0).^2/w.^2);
+function y = gaussian(x,x0,sigma)
+if nargin<2
+    error('Not enought input arguments.')
+end
+if nargin<3
+    sigma = 0.5;
+end
+%Calculate Gaussian function
+y = 1/sqrt(2*pi)*1/sigma*exp(-((x - x0)/sigma).^2);
+
 end
