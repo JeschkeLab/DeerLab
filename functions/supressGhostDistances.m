9d10ced9ee264bf661dd647e67458e5d65d6c48d
Luis Fabregas <luisf@student.ethz.ch>
2019-07-24 13:07:46 -0700

    suppressGhostDistances: no longer returns the dipolar evolution function but just the power scaled data.

--- functions/supressGhostDistances.m
+++ functions/supressGhostDistances.m
@@ -1,4 +1,4 @@
-function [DipEvoFcn] = supressGhostDistances(FormFactor,ModulationDepth,NRadicals)
+function Signal = supressGhostDistances(Signal,NRadicals)
 
 if nargin<3
     NRadicals = 2;
@@ -6,10 +6,7 @@ end
 
 Scaling = 1/(NRadicals - 1);
 
-FormFactor = FormFactor.^Scaling;
-
-DipEvoFcn = (FormFactor - (1 - ModulationDepth))/ModulationDepth;
-DipEvoFcn = DipEvoFcn/DipEvoFcn(1);
+Signal = Signal.^Scaling;
 
 end
 
