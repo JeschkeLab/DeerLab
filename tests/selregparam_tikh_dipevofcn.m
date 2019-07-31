9efad2c990e1225467201c99e248efaf4252b1f8
Luis Fabregas <luisf@student.ethz.ch>
2019-07-31 08:47:28 -0700

    minor bug in test fixed

--- tests/selregparam_tikh_dipevofcn.m
+++ tests/selregparam_tikh_dipevofcn.m
@@ -20,7 +20,7 @@ OptParam = selregparam(RegParamSet,DipEvoFcn,Kernel,RegMatrix,{'aic','gcv','lr'}
 
 %Accept testif all values are the same (should be as there is no noise)
 err(1) = any(diff(OptParam) > 1e-2);
-err(2) = any(abs(OptParam - 0.002));
+err(2) = any(abs(OptParam - 0.002) > 1e-4);
 err = any(err);
 data = [];
 
