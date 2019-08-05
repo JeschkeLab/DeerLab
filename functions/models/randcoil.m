ef168c49afac237253e8b4ec8729e4e9c69ac01f
Luis Fabregas <luisf@student.ethz.ch>
2019-08-05 09:46:26 -0700

    implemented random coil parametric model, test passed

---
+++ functions/models/randcoil.m
@@ -0,0 +1,57 @@
+function output = randcoil(r,param)
+
+% Random-coil model for an unfolded peptide/protein, end-to-end distance
+% distribution is approximated by a Gaussian coil with proper mean
+% distance, which is good for sufficiently large N
+% N. C. Fitzkee, G. D. Rose, PNAS 2004, 101(34), 12497-12502
+%
+% PARAMETERS
+% name    symbol default lower bound upper bound
+% par(1)  N      50      2              1000    number of residues between labels, including labeled residues
+% par(2)  nu     0.602   0.33           1       scaling exponent
+
+R0=0.198; % 1.98 Å per residue
+nParam = 2;
+
+if nargin==0
+    %If no inputs given, return info about the parametric model
+    info.Model  = 'Random-coil model';
+    info.Equation  = ['3/(2*pi*3/(2*pi*6.r0*N*',char(957),')^(3/2)))^(3/2)*4*pi*r.^2*exp(-3*r.^2/(3/(2*pi*6.r0*N*',char(957),')^(3/2))'];
+    info.nParam  = nParam;
+    info.parameters(1).name = 'Chain members N';
+    info.parameters(1).range = [2 1000];
+    info.parameters(1).default = 50;
+    info.parameters(1).units = '';
+    
+    info.parameters(2).name = ['Scaling exponent',char(957)];
+    info.parameters(2).range = [0.33 1];
+    info.parameters(2).default = 0.602;
+    info.parameters(2).units = '';
+    
+    output = info;
+    
+elseif nargin == 2
+    
+    %If user passes them, check that the number of parameters matches the model
+    if length(param)~=nParam
+        error('The number of input parameters does not match the number of model parameters.')
+    end
+    
+    SquareDist = 6*R0*param(1)^param(2)^2; %mean square end-to-end distance from radius of gyration
+    normFact = 3/(2*pi*SquareDist)^(3/2); % normalization prefactor
+    ShellSurf = 4*pi*r.^2; % spherical shell surface
+    Gaussian = exp(-3*r.^2/(2*SquareDist));
+    Distribution = normFact*ShellSurf.*Gaussian;
+    
+    %Normalize integral
+    Distribution = Distribution/sum(Distribution);
+    output = Distribution;
+    
+else
+    
+    %Else, the user has given wrong number of inputs
+    error('Model requires two input arguments.')
+end
+
+return
+
---
+++ tests/fitparamodel_randcoil_dipevo.m
@@ -0,0 +1,37 @@
+function [err,data,maxerr] = test(opt,oldata)
+
+
+Dimension = 500;
+TimeStep = 0.008;
+TimeAxis = linspace(0,TimeStep*Dimension,Dimension);
+DistanceAxis = time2dist(TimeAxis);
+InputParam = [20 0.49];
+R0=0.198; % 1.98 Å per residue
+SquareDist = 6*R0*InputParam(1)^InputParam(2)^2; %mean square end-to-end distance from radius of gyration
+normFact = 3/(2*pi*SquareDist)^(3/2); % normalization prefactor
+ShellSurf = 4*pi*DistanceAxis.^2; % spherical shell surface
+Gaussian = exp(-3*DistanceAxis.^2/(2*SquareDist));
+Distribution = normFact*ShellSurf.*Gaussian;
+Distribution = Distribution/sum(Distribution);
+Distribution = Distribution.';
+
+Kernel = dipolarkernel(TimeAxis,DistanceAxis);
+DipEvoFcn = Kernel*Distribution;
+
+[FitDistribution] = fitparamodel(DipEvoFcn,Kernel,DistanceAxis,@randcoil);
+err = any(abs(FitDistribution - Distribution)>1e-10);
+
+maxerr = max(abs(FitDistribution - Distribution));
+data = [];
+
+if opt.Display
+   figure(1),clf,
+   subplot(121),hold on
+   plot(TimeAxis,DipEvoFcn,'b')
+   plot(TimeAxis,Kernel*FitDistribution,'r')
+   subplot(122),hold on
+   plot(DistanceAxis,Distribution,'b')
+   plot(DistanceAxis,FitDistribution,'r')
+end
+
+end
