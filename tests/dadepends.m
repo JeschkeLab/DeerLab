c2f7ee52ed08f02e170b1607b277d04ada3f0c84
Luis Fabregas <luisf@student.ethz.ch>
2019-08-07 09:31:25 -0700

    added function for toolbox dependency analysis and display similar to datest

---
+++ tests/dadepends.m
@@ -0,0 +1,67 @@
+function dadepends(FileMask)
+
+DeerAnalysisPath = fileparts(which('DeerAnalysis'));
+if isempty(DeerAnalysisPath)
+    error('DeerAnalysis is not on the Matlab path!');
+end
+
+FileMask = [FileMask '*'];
+
+fid = 1;
+fprintf(fid,'=======================================================================\n');
+fprintf(fid,'DeerAnalysis Toolbox Dependency Analysis           %s\n(Matlab %s)\n',datestr(now),version);
+fprintf(fid,'DeerAnalysis location: %s\n',DeerAnalysisPath);
+fprintf(fid,'=======================================================================\n');
+
+%Run through ./tests/
+files = dir(fullfile('.',FileMask));
+filenames = {files.name};
+fprintf(fid,'-----------------------------------------------------------------------\n');
+fprintf(fid,'Path: /tests/                                                          \n');
+fprintf(fid,'-----------------------------------------------------------------------\n');
+fprintf(fid,'Filename                              Dependencies                   \n');
+
+for i=1:length(filenames)
+    
+    output = dependencies.toolboxDependencyAnalysis({filenames{i}});
+    
+    if ~isempty(output)
+        string = output{1};
+        for j=2:length(output)
+            string = [string ', ' output{j}];
+        end
+        
+        fprintf(fid,'%-36s  %s \n',filenames{i},string);
+    end
+end
+
+%Run through ./functions/
+files = dir(fullfile('../functions',FileMask));
+filenames = {files.name};
+
+fprintf(fid,'-----------------------------------------------------------------------\n');
+fprintf(fid,'Path: /functions/                                                          \n');
+fprintf(fid,'-----------------------------------------------------------------------\n');
+fprintf(fid,'Filename                              Dependencies                   \n');
+
+for i=1:length(filenames)
+    
+    output = dependencies.toolboxDependencyAnalysis({fullfile('../functions/',filenames{i})});
+    
+    if ~isempty(output)
+        string = output{1};
+        for j=2:length(output)
+            string = [string ', ' output{j}];
+        end
+        
+        fprintf(fid,'%-36s  %s \n',filenames{i},string);
+    end
+        
+end
+
+
+fprintf(fid,'=======================================================================\n');
+
+end
+
+
