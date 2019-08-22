c87211abc18e112ba495d778269302fc1b9b2531
Luis Fabregas <luisf@student.ethz.ch>
2019-08-21 17:05:16 -0700

    confy.py: deleted unused code

--- docs/docsrc/source/conf.py
+++ docs/docsrc/source/conf.py
@@ -25,13 +25,8 @@ sys.path.append(os.path.abspath('./demo/'))
 
 from sphinx.locale import _
 
-from sphinx_rtd_theme import __version__
-
-
 project = u'Read the Docs Sphinx Theme'
 slug = re.sub(r'\W+', '-', project.lower())
-version = __version__
-release = __version__
 author = u'Dave Snider, Read the Docs, Inc. & contributors'
 copyright = author
 language = 'en'
