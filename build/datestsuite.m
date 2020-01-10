6cbedc44b5f6a8206c033ed9a42078d52479712e
Luis Fabregas <luisf@student.ethz.ch>
2020-01-10 16:22:07 +0100

    ci.yml: python script now executed from ubuntu, due to bug in matlab with python libraries

--- build/datestsuite.m
+++ build/datestsuite.m
@@ -21,8 +21,8 @@ if ispc
     system('python uploadS3.py --keyfile %USERPROFILE%\.ssh\aws_access_keys.txt --file "coverage_badge.json" --bucket deershields')
     system('python uploadS3.py --keyfile %USERPROFILE%\.ssh\aws_access_keys.txt --file "testsuite_badge.json" --bucket deershields')
 elseif isunix
-    system('python3 uploadS3.py --keyfile ~/.ssh/aws_access_keys.txt --file "coverage_badge.json" --bucket deershields')
-    system('python3 uploadS3.py --keyfile ~/.ssh/aws_access_keys.txt --file "testsuite_badge.json" --bucket deershields')
+    %system('python3 uploadS3.py --keyfile ~/.ssh/aws_access_keys.txt --file "coverage_badge.json" --bucket deershields')
+    %system('python3 uploadS3.py --keyfile ~/.ssh/aws_access_keys.txt --file "testsuite_badge.json" --bucket deershields')
 elseif ismac
     error('macOS is not supported')
 end
