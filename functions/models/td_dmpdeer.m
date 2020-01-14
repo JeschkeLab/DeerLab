294eb24e0c47008e49a1e9e1007cfcc6fc26de7f
Luis Fabregas <luisf@student.ethz.ch>
2020-01-14 16:19:09 +0100

    norepcomb function implemented as local function rather than private

--- functions/models/td_dmpdeer.m
+++ functions/models/td_dmpdeer.m
@@ -118,3 +118,31 @@ end
 
     %Apply the background
     V = V.*B;
+
+    
+    return
+    
+    
+function output = norepcomb(vals,taken)
+
+[~,Nvals] = size(vals);
+
+Nrows = 2.^(Nvals);
+Ncycles = Nrows;
+
+for i = 1:Nvals
+    settings = (0:1);
+    Ncycles = Ncycles/2;
+    nreps = Nrows./(2*Ncycles);
+    settings = settings(ones(1,nreps),:);
+    settings = settings(:);
+    settings = settings(:,ones(1,Ncycles));
+    Indexes(:,Nvals-i+1) = settings(:);
+end
+
+Index = Indexes(sum(Indexes,2) == taken,:);
+nrows = size(Index,1);
+[Nrows,~] = find(Index');
+output = reshape(vals(Nrows),taken,nrows).';
+
+return
