359744c27da09709353f42b1f18033003264688e
Luis Fabregas <luisf@student.ethz.ch>
2020-01-30 11:52:47 +0100

    added tests for Gauss/Rician parametric models

---
+++ tests/models_rd_threerice.m
@@ -0,0 +1,27 @@
+function [err,data,maxerr] = test(opt,olddata)
+
+
+r = linspace(0,50,500);
+
+%Control row/column effect
+P1 = rd_threerice(r  ,[2.5 0.4 4.0 0.4 5.0 0.4 0.3 0.3]);
+P2 = rd_threerice(r.',[2.5 0.4 4.0 0.4 5.0 0.4 0.3 0.3]);
+err(1) = ~isequal(P1,P2);
+
+%Control non-negativity
+err(2) = any(P1<0);
+
+P3 = rd_threerice(r,[1.0 0.1 1.0 0.1 1.0 0.1 0.3 0.3]);
+P4 = rd_threerice(r,[10 5 10 5 10 5 0.3 0.3]);
+
+%Control non-negativity ov default boundaries
+err(3) = any(P3<0) | any(P4<0);
+
+%Control there are no NaN
+err(4) = any (isnan(P1)) | any(isnan(P2)) | any(isnan(P3)) | any(isnan(P4));
+
+err = any(err);
+data = [];
+maxerr = NaN;
+
+end
