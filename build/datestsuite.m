46ad9a1091f2f4d4446bb9ee751bf3fdbc2c376b
Luis Fabregas <luisf@student.ethz.ch>
2020-01-31 14:57:56 +0100

    datestsuite: when running local on a WinOS, delete the artifacts after uploading

--- build/datestsuite.m
+++ build/datestsuite.m
@@ -16,6 +16,11 @@ end
 if ispc
     system('python uploadS3.py --keyfile %USERPROFILE%\.ssh\aws_access_keys.txt --file "coverage_badge.json" --bucket deershields')
     system('python uploadS3.py --keyfile %USERPROFILE%\.ssh\aws_access_keys.txt --file "testsuite_badge.json" --bucket deershields')
+
+    %Delete files once uploaded
+    delete('coverage_badge.json')
+    delete('testsuite_badge.json')
+
 elseif isunix
     %system('python3 uploadS3.py --keyfile ~/.ssh/aws_access_keys.txt --file "coverage_badge.json" --bucket deershields')
     %system('python3 uploadS3.py --keyfile ~/.ssh/aws_access_keys.txt --file "testsuite_badge.json" --bucket deershields')
