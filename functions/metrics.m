30eaa88edb542fb737e1fd0b825018d3607922a8
Stefan Stoll <stst@uw.edu>
2020-02-17 10:35:34 -0800

    metrics: remove duplicate code

--- functions/metrics.m
+++ functions/metrics.m
@@ -1,57 +1,59 @@
-function metric = metrics(pointA,pointB,metricName) 
+function metric = metrics(A,B,metricName) 
+
 if nargin<3
    metricName = 'all'; 
 end
 
-  switch metricName
-      case 'overlap'
-          metric = 1 - sum(min(pointA,pointB));
-      case 'determination'
-          metric = sum((pointA - pointB).^2)/sum((pointA - mean(pointA)).^2);
-      case 'chebyshev'
-          metric = max(abs(pointA - pointB));
-      case 'braycurtis'
-          metric = 1 - dot(pointA - mean(pointA),pointB - mean(pointB))/(norm(pointB - mean(pointB))*norm(pointA - mean(pointA)));
-      case 'cosine'
-          metric = norm(pointA - pointB,1)/norm(pointA + pointB,1);
-      case 'correlation'
-          metric = 1 - dot(pointA,pointB)/(norm(pointB)*norm(pointA));
-      case 'chi'
-          metric = sum((pointA - pointB).^2./(pointA + pointB));
-      case 'bregman'
-          metric = 1/2*norm(pointA - pointB) + dot(pointA, pointA - pointB);
-      case 'mad'
-          metric = mean(abs(pointA - pointB));
-      case 'msd'
-          metric = mean(abs(pointA - pointB).^2);
-      case 'rmsd'
-          metric = sqrt(1/numel(pointA)*norm(pointA - pointB)^2);
-      case 'nrmsd'
-          metric = sqrt(mean(abs(pointA - pointB)))/(max(pointA) - min(pointA));
-      case 'hellinger'
-          metric = sqrt(1 - sum(sqrt(pointA.*pointB) / sqrt(sum(pointA) * sum(pointB))));
-      case 'euclidean'
-          metric  = norm(pointA - pointB);
-      case 'bhattacharyya'
-          metric  = -log(sum(sqrt(pointA.*pointB))/sqrt(sum(pointA))/sqrt(sum(pointB)));
-      case 'tv'
-          metric =  1/2*norm(pointA - pointB,1);
-      otherwise
-          metric.Overlap = sum(min(pointA/sum(pointA),pointB/sum(pointB)));
-          metric.Determination = sum((pointA - pointB).^2)/sum((pointA - mean(pointA)).^2);
-          metric.Chebyshev = max(abs(pointA - pointB));
-          metric.BrayCurtis = norm(pointA - pointB,1)/norm(pointA + pointB,1);
-          metric.Cosine = 1 - dot(pointA,pointB)/(norm(pointB)*norm(pointA));
-          metric.Correlation = 1 - dot(pointA - mean(pointA),pointB - mean(pointB))/(norm(pointB - mean(pointB))*norm(pointA - mean(pointA)));
-          metric.Chi = sum((pointA - pointB).^2./(pointA + pointB));
-          metric.Bregman = 1/2*norm(pointA - pointB) + dot(pointA, pointA - pointB);
-          metric.MeanAbsDev = mean(abs(pointA - pointB));
-          metric.MeanSquaredDev = mean(abs(pointA - pointB).^2);
-          metric.RMSDdeviation = sqrt(mean(abs(pointA - pointB)));
-          metric.normalizedRMSDdeviation = sqrt(mean(abs(pointA - pointB)))/(max(pointA) - min(pointA));
-          metric.Euclidean  = norm(pointA - pointB);
-          metric.Hellinger = sqrt(1 - sum(sqrt(pointA.*pointB) / sqrt(sum(pointA) * sum(pointB))));
-          metric.TotalVariation =  1/2*norm(pointA - pointB,1);
-          metric.Bhattacharyya  = -log(sum(sqrt(pointA.*pointB))/sqrt(sum(pointA))/sqrt(sum(pointB)));         
-  end
+switch metricName
+    case 'overlap'
+        metric = 1 - sum(min(A,B));
+    case 'determination'
+        metric = sum((A - B).^2)/sum((A - mean(A)).^2);
+    case 'chebyshev'
+        metric = max(abs(A - B));
+    case 'braycurtis'
+        metric = 1 - dot(A - mean(A),B - mean(B))/(norm(B - mean(B))*norm(A - mean(A)));
+    case 'cosine'
+        metric = norm(A - B,1)/norm(A + B,1);
+    case 'correlation'
+        metric = 1 - dot(A,B)/(norm(B)*norm(A));
+    case 'chi'
+        metric = sum((A - B).^2./(A + B));
+    case 'bregman'
+        metric = 1/2*norm(A - B) + dot(A, A - B);
+    case 'mad'
+        metric = mean(abs(A - B));
+    case 'msd'
+        metric = mean(abs(A - B).^2);
+    case 'rmsd'
+        metric = sqrt(1/numel(A)*norm(A - B)^2);
+    case 'nrmsd'
+        metric = sqrt(mean(abs(A - B)))/(max(A) - min(A));
+    case 'hellinger'
+        metric = sqrt(1 - sum(sqrt(A.*B) / sqrt(sum(A) * sum(B))));
+    case 'euclidean'
+        metric  = norm(A - B);
+    case 'bhattacharyya'
+        metric  = -log(sum(sqrt(A.*B))/sqrt(sum(A))/sqrt(sum(B)));
+    case 'tv'
+        metric =  1/2*norm(A - B,1);
+    otherwise
+        metric.Overlap = metrics(A,B,'overlap'); 
+        metric.Determination = metrics(A,B,'determination');
+        metric.Chebyshev = metrics(A,B,'chebyshev'); 
+        metric.BrayCurtis = metrics(A,B,'braycurtis');
+        metric.Cosine = metrics(A,B,'cosine');
+        metric.Correlation = metrics(A,B,'correlation');
+        metric.Chi = metrics(A,B,'chi');
+        metric.Bregman = metrics(A,B,'bregman');
+        metric.MeanAbsDev = metrics(A,B,'mad');
+        metric.MeanSquaredDev = metrics(A,B,'msd');
+        metric.RMSDdeviation = metrics(A,B,'rmsd');
+        metric.normalizedRMSDdeviation = metrics(A,B,'nrmsd');
+        metric.Euclidean = metrics(A,B,'euclidean');
+        metric.Hellinger = metrics(A,B,'hellinger');
+        metric.Bhattacharyya  = metrics(A,B,'bhattacharyya');
+        metric.TotalVariation =  metrics(A,B,'tv');
+end
+
 end
--- tests/metrics_values.m
+++ tests/metrics_values.m
@@ -8,23 +8,24 @@ metricNames = {'overlap','determination','chebyshev','cosine',...
     'correlation','chi','bregman','mad','msd','rmsd', 'nrmsd',...
     'hellinger','euclidean', 'braycurtis','bhattacharyya','tv'};
 
-rng(2)
+rng(2);
+
 trial = rand(20,1);
-trial  = trial/sum(trial);
+trial = trial/sum(trial);
 truth = trial;
 
-for i=1:length(metricNames)
-values(i) = metrics(trial,truth,metricNames{i});
+for i = 1:length(metricNames)
+    values(i) = metrics(trial,truth,metricNames{i});
 end
-values2 = metrics(trial,truth);
+values_all = metrics(trial,truth);
 
 err(1)  = any(values>1e-7);
 err(2)  = any(~isreal(values));
-err(3) = ~isstruct(values2);
+err(3) = ~isstruct(values_all);
+
 err = any(err);
 maxerr = NaN;
 data = [];
-
 
 end
 
