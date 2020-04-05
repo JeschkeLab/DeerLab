cae2a5c8d33b08e9fb7680b4708eaacd2f672196
Stefan Stoll <stst@uw.edu>
2020-04-05 14:19:15 -0700

    mixmodels: use for loop instead of nested function handles

--- functions/mixmodels.m
+++ functions/mixmodels.m
@@ -38,72 +38,66 @@ nModels = numel(models);
 % Combine the information structures of the models
 %-------------------------------------------------------------------------------
 Info.model = 'Mixed model';
+Info.models = {};
 
 % Add amplitudes for each model except last
 for j = 1:nModels-1
-    Info.parameters(j).name = sprintf('Relative amplitude of model #%i',j);
+    Info.parameters(j).name = sprintf('Component %i: Relative amplitude',j);
     Info.parameters(j).range = [0 1];
     Info.parameters(j).default = 1/nModels;
     Info.parameters(j).units = '';
 end
-Info.nparam = nModels-1; % amplitudes
+pidx_amp = 1:nModels-1;
 
-% Combine parameters for each model into an array
-paramsplit = zeros(numel(models),1);
-paramsplit(1) = Info.nparam;
-
-% Add information structure from each model
-for i = 1:length(models)
+% Combine info structures from all models
+idx = pidx_amp(end);
+for i = 1:nModels
     info = models{i}();
-    paramsplit(i+1) =  paramsplit(i) + info.nparam;
+    pidx{i} = idx + (1:info.nparam);
+    idx = idx + info.nparam;
     Info.models{i} = info.model;
+    param_ = info.parameters;
     for j = 1:info.nparam
-        Info.parameters(length(Info.parameters)+1).name = sprintf('%s of model #%i',info.parameters(j).name,i);
-        Info.parameters(length(Info.parameters)).range = info.parameters(j).range;
-        Info.parameters(length(Info.parameters)).default = info.parameters(j).default;
-        Info.parameters(length(Info.parameters)).units = info.parameters(j).units;
+        param_(j).name = sprintf('Component %i: %s',i,param_(j).name);
     end
-    Info.nparam =  Info.nparam + info.nparam;
+    Info.parameters = [Info.parameters param_];
 end
 
-% Merge the parametric model functions
+Info.nparam = numel(Info.parameters);
+
+% Mixed model function handle
 %-------------------------------------------------------------------------------
-
-% Start with a dummy function handle
-mixedModel = @(r,param) 0*r;
-% Add models one by one using nested function handles
-for j = 1:nModels-1
-    currentModel = models{j};
-    mixedModel = @(r,param) (mixedModel(r,param) + param(j)*currentModel(r,param(paramsplit(j)+1:paramsplit(1+j))) );
-end
-% For the last model use constrained amplitude
-currentModel = models{end};
-mixedModel = @(r,param) (mixedModel(r,param) + max(1 - sum(param(1:paramsplit(1))),0)*currentModel(r,param(paramsplit(end-1)+1:paramsplit(end))) );
-
-% Bundle everyhting into a function handle which can handle varargin cases
 mixModelFcn = @mixedFunction;
 
     % Function to allow request of information structure or model values
-    function output = mixedFunction(varargin)
+  function output = mixedFunction(varargin)
 
         if nargin==0
             output = Info;
             return
-        else
-            if nargin<2
-                error('At least two input argumetns (ax,param) are required.')
-            elseif  nargin>3
-                error('Too many input argumetns given.')
-            end
-        
-            r = varargin{1};
-            if ~iscolumn(r)
-               r = r.'; 
-            end
-            distr = mixedModel(r,varargin{2});
-            output = distr;
         end
         
+        if nargin<2
+            error('At least two input arguments (r,param) are required.')
+        elseif  nargin>3
+            error('Only two input arguments are allows.')
+        end
+        
+        x = varargin{1};
+        params = varargin{2};
+        if ~iscolumn(x)
+            x = x.';
+        end
+        
+        amp = params(pidx_amp);
+        amp(end+1) = max(1-sum(amp),0);
+        
+        y = 0;
+        for k = 1:numel(models)
+            y = y + amp(k)*models{k}(x,params(pidx{k}));
+        end
+        output = y;
+        
     end
-
+    
 end
