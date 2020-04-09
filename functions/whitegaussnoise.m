869bf4750341d410639cc110c3e7976f27e2ab7d
Stefan Stoll <stst@uw.edu>
2020-04-09 15:16:48 -0700

    whitegaussnoise: remove rescaling

--- functions/whitegaussnoise.m
+++ functions/whitegaussnoise.m
@@ -11,31 +11,22 @@
 %   The time axis (t) can be passed as well as an argument instead of the
 %   number of points.
 %
-%
 
 % This file is a part of DeerLab. License is MIT (see LICENSE.md). 
 % Copyright(c) 2019-2020: Luis Fabregas, Stefan Stoll and other contributors.
 
 
-function ampnoise = whitegaussnoise(N,level)
+function noise = whitegaussnoise(N,level)
 
 if numel(N)>1
-   N = numel(N); 
+   N = numel(N);
 end
 
-%Validate input
+% Validate input
 validateattributes(N,{'numeric'},{'scalar','nonnegative','nonempty'},mfilename,'N')
 validateattributes(level,{'numeric'},{'scalar','nonnegative','nonempty'},mfilename,'seed')
 
-%Generate Gaussian noise
-noise = randn(N,1);
-%Increase amplitude of the noise vector until standard deviation matches
-%the requested noise level
-ampnoise = 0*noise;
-amp = 0;
-while std(ampnoise)<level
-    amp = amp + level/100;
-    ampnoise = amp*noise;
-end
+% Generate Gaussian noise
+noise = level*randn(N,1);
 
 end
