b85f93c8337b8e783be2a06b9c0b252466b02639
Luis Fabregas <luisf@student.ethz.ch>
2020-04-13 15:10:58 +0200

    adapted failing test due to recent changes

--- tests/selregparam_absolute.m
+++ tests/selregparam_absolute.m
@@ -14,9 +14,9 @@ alphaopt1 = selregparam(S,K,r,'tikhonov',methods,'NonNegConstrained',false,'Nois
 methods = {'cv','gcv','rgcv','srgcv','aic','bic','aicc','rm','ee','ncp','gml','mcl'};
 alphaopt2 = selregparam(S,K,r,'tikhonov',methods,'NonNegConstrained',false,'NoiseLevel',0.05,'search','golden');
 
-% Values returned by the selregparam function at commit 8918a091ca350bb7fc04febff870e385f169a7f7   (10/03/2020)
-alpharef1 = [0.5012 0.5012   79.4328 0.3162 0.3162 1.9953 0.3162 0.6310 0.3162 1.0000   25.1189 0.7943 0.3162 1.0000];
-alpharef2 = [79.1674 0.3215 0.3355 1.7724 0.2975 0.5711 0.3445 0.9753   25.8199 0.7562 0.3475 0.9532];
+% Values returned by the selregparam function at commit cec7a2b5af55a10739ec7993dd1aee4c25920f6c  (13/04/2020)
+alpharef1 = [0.5012 0.5012   79.4328 0.3162 0.3162 1.5849 0.3162 0.6310 0.3162 1.0000 25.1189 0.7943 0.3162 1.0000];
+alpharef2 = [79.1674 0.3205 0.3330 1.7510 0.2959 0.5680 0.3426 0.9668   25.5417 0.7521 0.3445 0.9551];
 
 % Pass 1: all values have the same absolute values
 pass(1) = all(abs(alpharef1 - alphaopt1)./alpharef1 < 1e-3);
