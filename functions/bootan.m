896b87106471ffaf26aaf916e959373089653b74
Luis Fabregas <luisf@student.ethz.ch>
2020-04-24 14:03:03 +0200

    bootan: now returns confidence intervals, added help

--- functions/bootan.m
+++ functions/bootan.m
@@ -1,9 +1,61 @@
+%
+% BOOTAN Bootstrap analysis for uncertainty estimation
+%
+%   stats = BOOTAN(fcn,V,Vfit)
+%   stats = BOOTAN(fcn,V,Vfit,Nsamples)
+%   stats = BOOTAN(___,'Name',value,___)
+%
+%   Performs a bootstrap analysis of the output variables returned by the
+%   function (fcn) given an experimental signal (V) and its fit (Vfit).
+%
+%   Inputs:
+%     fcn        function handle to the data analysis function, must be 
+%                accept V as sole input: fcn = @(V) myfunction(___,V,___)
+%     V          experimental signal
+%     Vfit       fitted signal
+%     Nsamples   number of bootstrap samples to evaluate (default=1000)
+%
+%  Name-value pairs:
+%
+%     'Verbose'    - Display progress information on the command window (true/false)
+%     'Resampling' - Resampling method for bootstrap samples
+%                        'gaussian' - sample noise from a Gaussian distribution (default)
+%                        'residual' - sample noise from the fit residuals
+%
+%   Outputs:
+%     stats     structure array with summary statistics for each variable
+%       .median medians of the output variables
+%       .mean   means of the output variables
+%       .std    standard deviations of the output variables
+%       .p1     2nd  percentiles of the output variables
+%       .p25    25th percentiles of the output variables
+%       .p75    75th percentiles of the output variables
+%       .p99    98th percentiles of the output variables
+%       .ci99   99%-Confidence intervals (percentile-based)
+%       .ci95   95%-Confidence intervals (percentile-based)
+%       .ci50   50%-Confidence intervals (percentile-based)
+%       .boothist    structure containing histogram data of the variable distributions
+%                        .edges  Histogram edges (X-data)
+%                        .bins   Histogram bins (in probability) (Y-data)
+%       .bootdist    structure containing the kernel-density estimation
+%                    distribution of the histogram
+%                        .values   Evaluated variable values
+%                        .pdf      Estimated probability density function
+%
+%   The .bootdist and .boothist fields are only computed for non-vectorial
+%   variables, e.g. model parameters, but not for vectorial variables, e.g.
+%   parameter-free distance distributions, backgrounds,...
+%
+
+% This file is a part of DeerLab. License is MIT (see LICENSE.md).
+% Copyright(c) 2019-2020: Luis Fabregas, Stefan Stoll and other contributors.
+
 function stats = bootan(fcn,Vexp,Vfit,nSamples,varargin)
 
 % Parse input scheme: bootan(fcn,Vexp,Vfit,'prop',value,___)
 if nargin>=4 && ischar(nSamples)
-   varargin = [{nSamples} varargin];
-   nSamples = [];
+    varargin = [{nSamples} varargin];
+    nSamples = [];
 end
 
 % Parse the optional parameters in varargin
@@ -34,7 +86,7 @@ if numel(Vexp)~=numel(Vfit)
 end
 
 if ~isa(fcn,'function_handle')
-   error('The 1st input must be a valid function handle of the form @(V)fcn(__,V,__)') 
+    error('The 1st input must be a valid function handle of the form @(V)fcn(__,V,__)')
 end
 
 % Determine the number of outputs returned by function handle and allocate arrays
@@ -71,7 +123,7 @@ for iSample = 1:nSamples
                 Vresample =  Vfit + residuals(randperm(numel(Vfit)));
         end
     else
-        % Use original data
+        % Use original data on the first run
         Vresample = Vexp;
     end
     
@@ -129,24 +181,35 @@ for iOut = 1:numel(evals)
         stats{iOut}(i).mean = means(i);
         stats{iOut}(i).median = medians(i);
         stats{iOut}(i).std = stds(i);
-        stats{iOut}(i).p2 = percentile(booti,2,1);
+        
+        % Percentiles
+        stats{iOut}(i).p1  = percentile(booti,1,1);
         stats{iOut}(i).p25 = percentile(booti,25,1);
         stats{iOut}(i).p75 = percentile(booti,75,1);
-        stats{iOut}(i).p98 = percentile(booti,98,1);
+        stats{iOut}(i).p99 = percentile(booti,99,1);
+       
+        % Confidence intervals
+        stats{iOut}(i).ci50(:,1) = stats{iOut}(i).p25;
+        stats{iOut}(i).ci50(:,2) = stats{iOut}(i).p75;
+        stats{iOut}(i).ci95(:,1) = percentile(booti,2.5,1);
+        stats{iOut}(i).ci95(:,2) = percentile(booti,97.5,1);
+        stats{iOut}(i).ci99(:,1) = percentile(booti,0.5,1);
+        stats{iOut}(i).ci99(:,2) = percentile(booti,99.5,1);
         
         %Compute the bootstrapped distributions for non-vectorial variables
         if nParam(iOut)<20
-            % Kernel-density estimations
-            xmin = 0.9*stats{iOut}(i).p2;
-            xmax = 1.1*stats{iOut}(i).p98;
+            
+            % Kernel-density estimation
+            xmin = 0.9*stats{iOut}(i).p1;
+            xmax = 1.1*stats{iOut}(i).p99;
             if all(diff(booti)==0)
-                y = 1;
-                x = 0;
+                pdf = 1;
+                values = 0;
             else
-                [~,y,x] = kde(booti,100,xmin,xmax);
+                [~,pdf,values] = kde(booti,100,xmin,xmax);
             end
-            stats{iOut}(i).bootdist.x = x;
-            stats{iOut}(i).bootdist.y = y;
+            stats{iOut}(i).bootdist.values = values;
+            stats{iOut}(i).bootdist.pdf = pdf;
             
             %Determine optimal bins for histogram via Freedman-Diaconis rule
             nbins = round(range(booti)/(2*iqr(booti)/(numel(booti)).^(1/3)),0);
