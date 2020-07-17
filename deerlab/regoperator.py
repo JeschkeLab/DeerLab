5759397aad0da0e3073eb68b869ac88c36279880
Luis Fabregas <luis.fabregas@phys.chem.ethz.ch>
2020-07-17 10:23:48 +0200

    implemented regparamrange

--- deerlab/regoperator.py
+++ deerlab/regoperator.py
@@ -5,4 +5,6 @@ def regoperator(r,n=2):
     N = np.size(r)
     L = np.eye(N,N+n)
     L = np.diff(L,n)
+    L = L.T
+    L = L[0:len(r)-n,:]
     return L
---
+++ test/test_regparamrange.py
@@ -0,0 +1,35 @@
+
+import numpy as np
+from numpy import pi, inf, NaN
+from deerlab import dipolarkernel, regoperator, regparamrange
+
+def test_resolution():
+#=======================================================================
+    "Check resolution can be set up manually"
+    
+    t = np.linspace(0,3,100)
+    r = np.linspace(2,6,100)
+    K = dipolarkernel(t,r)
+    L = regoperator(r,2)
+
+    alpha = regparamrange(K,L,logres=0.1)
+
+    assert len(alpha)==84
+#=======================================================================
+
+
+def test_values():
+#=======================================================================
+    "Check that returned values are correct"
+    
+    t = np.linspace(0,3,100)
+    r = np.linspace(2,6,5)
+    K = dipolarkernel(t,r,integralop=False)
+    L = regoperator(r,2)
+
+    # Reference: using MATLAB gsvd()
+    alpharef = [0.3981,0.5012,0.6310,0.7943,1.0000,1.2589,1.5849,1.9953]
+    alpha = regparamrange(K,L)
+    print(alpha, alpharef)
+    assert np.all(abs(alpha - alpharef) < 1e-4)
+#=======================================================================
