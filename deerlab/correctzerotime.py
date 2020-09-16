32813f40305f593289eab5c9df018077996dbf26
Luis Fabregas <luis.fabregas@phys.chem.ethz.ch>
2020-09-16 17:50:22 +0200

    correctzerotime: corrected behaviour, function was rescaling experimental data to the maximum

--- deerlab/correctzerotime.py
+++ deerlab/correctzerotime.py
@@ -32,12 +32,14 @@ def correctzerotime(V,t):
     if len(t)!=len(V):
         raise TypeError('The dipolar signal (V) and time axis (t) must have the size.')
 
-    # Rescale to avoid overflow during integration later
-    V /= max(V)
+
     # Generate finely-grained interpolated signal and time axis
     resolution = 10
     t_ = np.linspace(min(t), max(t), (len(t)-1)*resolution+1)
     V_ = scp.interpolate.interp1d(t, np.real(V), kind='cubic')(t_)
+
+    # Rescale to avoid overflow during integration later
+    V_ /= max(V_)
 
     # Determine location of maximum
     idxmax = np.argmax(V_)
