Fix signal scaling and add background fit to fitsignal plots (#78)
* fitsignal: correct plotting behavior with arbitrary scaling, add background to plot

* fitsignal: add option to request scaled background fits

* fitsignal: improve verbose fit summary

* docs: update beginner's guide with new changes

* add missing dependency to docs PR CI workflow

* docs: fix failing examples

* fitsignal: add output for unmodulated contribution Vunmod including uncertainty, remove scaledBckg option

* fitsignal: fix bug in fitting due to object reference copy

* fitsignal: fix error

* ftisignal: add Vmod as output, return all outputs and ucnertainties scaled as Vexp

test/test_fitsignal.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 from numpy import inf
+import deerlab as dl
 from deerlab import dipolarkernel, whitegaussnoise, fitsignal
 from deerlab.dd_models import dd_gauss
 from deerlab.bg_models import bg_exp, bg_hom3d
@@ -532,6 +533,58 @@ def test_V_scale_regularized():
     assert max(abs(1 - V/fit.V)) < 1e-4
 # ======================================================================
 
+def test_physical_bg_model():
+# ======================================================================
+    "Check that the background parameters of a physical model are fitted correctly"
+
+    t = np.linspace(-0.1,7,200)
+    r = np.linspace(3,5,50)
+    P = dl.dd_gauss(r,[4,0.2])
+    V0 = 3000
+    K = dl.dipolarkernel(t,r,0.4,lambda t,lam:dl.bg_hom3d(t,50,lam))
+    V = K@P
+    V = V0*V
+
+    fit = dl.fitsignal(V,t,r,'P',dl.bg_hom3d,dl.ex_4pdeer,uqanalysis=False)
+
+    assert abs(fit.bgparam - 50)<1e-1 and abs(fit.exparam - 0.4)<1e-1
+# ======================================================================
+
+def test_phenomenological_bg_model():
+# ======================================================================
+    "Check that the background parameters of a phenomenological model are fitted correctly"
+
+    t = np.linspace(-0.1,7,200)
+    r = np.linspace(3,5,50)
+    P = dl.dd_gauss(r,[4,0.2])
+    V0 = 3000
+    K = dl.dipolarkernel(t,r,dl.ex_4pdeer(0.4),lambda t: dl.bg_exp(t,0.3))
+    V = K@P
+    V = V0*V
+
+    fit = dl.fitsignal(V,t,r,'P',dl.bg_exp,dl.ex_4pdeer,uqanalysis=False)
+
+    assert abs(fit.bgparam - 0.3)<1e-1 and abs(fit.exparam - 0.4)<1e-1
+# ======================================================================
+
+
+def test_Vunmod():
+# ======================================================================
+    "Check that the background scaling is correct if requested"
+
+    t = np.linspace(-0.1,7,200)
+    r = np.linspace(3,5,50)
+    P = dl.dd_gauss(r,[4,0.2])
+    lam = 0.4
+    K = dl.dipolarkernel(t,r,dl.ex_4pdeer(lam),lambda t,lam: dl.bg_hom3d(t,50,lam))
+    Bscaled = (1-lam)*dl.bg_hom3d(t,50,lam)
+    V = K@P
+
+    fit = dl.fitsignal(V,t,r,'P',dl.bg_exp,dl.ex_4pdeer,uqanalysis=False)
+
+    assert max(abs(Bscaled - fit.Vunmod))<1e-4
+# ======================================================================
+
 def test_cost_value():
 # ======================================================================
     "Check that starts values can be correctly specified"
@@ -548,4 +601,3 @@ def test_cost_value():
 
     assert isinstance(fit.cost,float) and np.round(fit.cost/np.sum(fit.residuals**2),5)==1
 # ======================================================================
-