059c8cfa9de59db6c82c4368d2b2522ab2665880
Luis Fabregas <luis.fabregas@phys.chem.ethz.ch>
2021-04-01 11:58:45 +0200

    fix test

--- examples/plot_emulating_deeranalysis_workflow.py
+++ examples/plot_emulating_deeranalysis_workflow.py
@@ -27,7 +27,7 @@ conc = 180 #uM
 
 # Simulate an experimental signal with some scale and phase
 Bmodel = lambda t, lam: dl.bg_hom3d(t,conc,lam)
-K = dl.dipolarkernel(t,rtrue,lam,Bmodel)
+K = dl.dipolarkernel(t,rtrue,mod=lam,bg=Bmodel)
 V = K@Ptrue*np.exp(1j*np.pi/16) # add a phase shift 
 rnoise = dl.whitegaussnoise(t,0.01,seed=1) # real-component noise 
 inoise = 1j*dl.whitegaussnoise(t,0.01,seed=2) # imaginary-component noise 
