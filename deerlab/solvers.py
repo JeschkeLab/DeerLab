snlls: fix scale-invariance of covariance uncertainty quantification

deerlab/solvers.py
@@ -720,6 +720,10 @@ def snlls(y, Amodel, par0=None, lb=None, ub=None, lbl=None, ubl=None, nnlsSolver
     # Uncertainty analysis
     #---------------------
     if uq:
+        
+        # Define the indices of the parameter subsets
+        nonlin_subset = np.arange(0,Nnonlin)
+        lin_subset = np.arange(Nnonlin,Nnonlin+Nlin)
 
         def uq_subset(uq_full,subset,subset_lb,subset_ub):
         #-----------------------------------------------------------------------------
@@ -730,32 +734,36 @@ def snlls(y, Amodel, par0=None, lb=None, ub=None, lbl=None, ubl=None, nnlsSolver
 
             return uq_subset
         #-----------------------------------------------------------------------------
-        
+
         # Jacobian (non-linear part)
         Jnonlin = Jacobian(_ResidualsFcn,nonlinfit,lb,ub)
         # Jacobian (linear part)
+        scale = np.trapz(linfit,ax)
         Jlin = weights[:,np.newaxis]*Amodel(nonlinfit)
         if includeExtrapenalty:
             for penalty in extrapenalty:
                 Jlin = np.concatenate((Jlin, Jacobian(lambda plin: penalty(nonlinfit,plin),linfit,lbl,ubl)))
         if includeRegularization:
             Jlin = np.concatenate((Jlin, _penalty_augmentation(alpha, L, linfit,'Jacobian')))
+        Jlin *= scale
         # Full Jacobian
-        J = np.concatenate((Jnonlin/np.max(y),Jlin),axis=1)
+        J = np.concatenate((Jnonlin,Jlin),axis=1)
 
         # Calculate the heteroscedasticity consistent covariance matrix
         covmatrix = hccm(J, res)
+
         # Get combined parameter sets and boundaries
         parfit = np.concatenate((nonlinfit, linfit))
         lbs = np.concatenate((lb, lbl))
         ubs = np.concatenate((ub, ubl))
 
+        # Account for scale of linear parameters since covariance matrix is scale invariant
+        covmatrix[np.ix_(lin_subset,lin_subset)] *= scale**2
+
         # Construct the uncertainty quantification object
         paramuq = UQResult('covariance', parfit, covmatrix, lbs, ubs)
 
         # Split the uncertainty quantification of nonlinear/linear parts
-        nonlin_subset = np.arange(0,Nnonlin)
-        lin_subset = np.arange(Nnonlin,Nnonlin+Nlin)
         paramuq_nonlin = uq_subset(paramuq,nonlin_subset,lb,ub)
         paramuq_lin = uq_subset(paramuq,lin_subset,lbl,ubl)
 

test/test_snlls.py
@@ -465,7 +465,7 @@ def test_confinter_values():
                 [1.8384482433779732, 2.0515346381926847], 
                 [1.8899306688797555, 1.9961521871803736]]
 
-    fit = snlls(y,A,[1,0.5],reg=False)
+    fit = snlls(y,A,[0.5,0.2],reg=False)
     a_ci = [fit.nonlinUncert.ci(cov[i])[0,:] for i in range(3)]
     b_ci = [fit.nonlinUncert.ci(cov[i])[1,:] for i in range(3)]
 
@@ -498,12 +498,21 @@ def test_confinter_scaling():
     fit1 = snlls(V*V0_1,lambda lam: dipolarkernel(t,r,mod=lam),nlpar0,lb,ub,lbl,nonlin_tol=1e-2)
     fit2 = snlls(V*V0_2,lambda lam: dipolarkernel(t,r,mod=lam),nlpar0,lb,ub,lbl,nonlin_tol=1e-2)
 
+    # Assess linear parameter uncertainties
     ci1 = fit1.linUncert.ci(95)
     ci2 = fit2.linUncert.ci(95)
     ci1[ci1==0] = 1e-16
     ci2[ci2==0] = 1e-16
 
-    assert np.max(abs(ci2/V0_2 - ci1)) < 0.002 
+    assert np.max(abs(ci2/V0_2 - ci1)) < 1e-6
+
+    # Assess nonlinear parameter uncertainties
+    ci1 = fit1.nonlinUncert.ci(95)
+    ci2 = fit2.nonlinUncert.ci(95)
+    ci1[ci1==0] = 1e-16
+    ci2[ci2==0] = 1e-16
+
+    assert np.max(abs(ci2 - ci1)) < 1e-6    
 #============================================================
 
 def test_global_weights():