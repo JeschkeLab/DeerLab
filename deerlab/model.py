695d64a9b7c068fe5fa4a4c7b4c31c9ecfbd3c7e
Luis Fabregas <48292540+luisfabib@users.noreply.github.com>
2021-11-22 20:01:22 +0100

    Fix merging with `addweights=True` of models with linked parameters (#245)
    * fix merging with addweights=True of models with linked parameters
    
    * remove constants from model signature check when addweights=True
    
    * fix errors introduced in previous commit

--- deerlab/model.py
+++ deerlab/model.py
@@ -1271,7 +1271,15 @@ def _combinemodels(mode,*inputmodels,addweights=False):
     if addweights:
 
         for n,(model,nonlinfcn) in enumerate(zip(models,[model.nonlinmodel for model in models])):
-            signature = model._nonlinsignature
+            constants = [constant['argkey'] for constant in model._constantsInfo]
+            signature = []
+            for param in model.signature: 
+                if param in constants:
+                    signature.append(param)                        
+                else:
+                    if not np.any(getattr(model,param).linear):
+                        signature.append(param)                        
+    
             signature.append('weight')
             def make_weighted_comb(nonlinfcn):
                 def weighted_comb(*inputargs):
