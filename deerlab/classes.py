553e205542650a1b2106c1c63089468cb1e46510
Luis Fabregas <48292540+luisfabib@users.noreply.github.com>
2021-12-10 20:06:56 +0100

    `UQResult`: fix median of bootstrap results (#254)
    * UQResult: fix median of bootstrap results
    
    * UQResult: fix bug for fixed parameters with Delta function bootstrap distributions

--- deerlab/classes.py
+++ deerlab/classes.py
@@ -171,7 +171,7 @@ class UQResult:
             covmat = np.squeeze(samples).T@np.squeeze(samples)/np.shape(samples)[0] - means*means.T
             self.mean = means 
             self.median = self.percentile(50)
-            self.median = np.array([nth_samples[n,0] if np.all(nth_samples==nth_samples[0]) else self.median[n] for n,nth_samples in enumerate(samples.T)])
+            self.median = np.array([nth_samples[0] if np.all(nth_samples==nth_samples[0]) else self.median[n] for n,nth_samples in enumerate(samples.T)])
             self.std = np.squeeze(np.std(samples,0))
             self.covmat = covmat
 
@@ -267,7 +267,7 @@ class UQResult:
             if np.all(samplen == samplen[0]):
                 # Dirac's delta distribution 
                 x = np.array([0.9*samplen[0],samplen[0],1.1*samplen[0]])
-                pdf = np.array([0,1,0])
+                pdf = np.array([0,1,0]).astype(float)
             else:
                 sigma = np.std(samplen, ddof=1)
                 bw = sigma*(len(samplen)*3/4.0)**(-1/5)
@@ -317,8 +317,9 @@ class UQResult:
         # Enforce non-negativity (takes care of negative round-off errors)
         pdf = np.maximum(pdf,0)
 
-        # Ensure normalization of the probability density function
-        pdf = pdf/np.trapz(pdf, x)
+        # Ensure normalization of the probability density function (if not a Dirac delta function)
+        if not np.all(pdf==np.array([0,1,0])):
+            pdf = pdf/np.trapz(pdf, x)
         
         return x, pdf
     #--------------------------------------------------------------------------------
