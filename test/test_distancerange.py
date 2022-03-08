337f45e385033bc8d21c8b276e616eaae426137d
Stefan Stoll <stst@uw.edu>
2022-03-08 00:09:13 -0800

    rename time2dist to distancerange

--- test/test_distancerange.py
+++ test/test_distancerange.py
@@ -1,23 +1,23 @@
 import numpy as np
-from deerlab import time2dist
+from deerlab import distancerange
 
-def test_time2dist_outsize():
+def test_distancerange_outsize():
     "Verify that the output has the correct size"
 
     t = np.linspace(-1,5,300)
-    rminmax = time2dist(t)
+    rminmax = distancerange(t)
     assert len(rminmax)==2
 
     nr = 200
-    r = time2dist(t,nr)
+    r = distancerange(t,nr)
     assert len(r)==nr
 
 
-def test_time2dist_rmin():
+def test_distancerange_rmin():
     "Verify that the minimum distance is correct"
 
     t, dt = np.linspace(-1, 5, 300, retstep=True)
-    rmin, _ = time2dist(t)
+    rmin, _ = distancerange(t)
 
     D = 52.04  # MHz nm^3
     nu_max = 1/dt/2/2*0.85  # Nyquist freq, with buffer
@@ -26,11 +26,11 @@ def test_time2dist_rmin():
     assert np.max(np.abs(rmin - rmin_ref)) < 1e-10
 
 
-def test_time2dist_rmax():
+def test_distancerange_rmax():
     "Verify that the maximum distance is correct"
 
     t, dt = np.linspace(-1, 5, 300, retstep=True)
-    _, rmax = time2dist(t)
+    _, rmax = distancerange(t)
 
     trange = max(t) - min(t)
     D = 52.04  # MHz nm^3
