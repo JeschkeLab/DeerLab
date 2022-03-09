3d0919c739f15645ee609273552612e20920c25c
Stefan Stoll <stestoll@users.noreply.github.com>
2022-03-08 23:53:11 -0800

    correctphase refactor (#279)

--- deerlab/correctphase.py
+++ deerlab/correctphase.py
@@ -1,89 +1,90 @@
 import numpy as np
-from deerlab.utils import isempty
 
-def correctphase(V, phase='posrealint', full_output=False):
-# ==========================================================================
+def correctphase(V, full_output=False):
     r"""
-    Phase correction of complex-valued data
+    Phase correction of complex-valued data.
 
-    Performs a phase optimization on the complex-valued data ``V`` by determining a phase
-    rotation of ``V`` that minimizes its imaginary component.
+    Rotates the phase of complex-valued data ``V`` to minimize the imaginary component.
+    Among the two phases that minimize the imaginary part, the one that gives a real
+    part with a positive average is used.
     
-    Two-dimensional datasets ``V2D``, e.g. from measurements with multiple scans, can be provided, 
-    and the phase correction will be done on each trace individually. The first dimension ``V2D[:,i]``
-    must contain the single traces. An array of phases ``phases`` can be specified to manually correct the traces.
+    For two-dimensional datasets ``V2D``, e.g. from measurements with multiple scans,
+    each slice ``V2D[:,i]`` is phase-rotated independently.
+    
 
     Parameters
     ----------
     V : array_like, or list of array_like
-        Complex-valued signals or list thereof.
-
-    phase : string, optional
-        Criterion for selection of correction phase. 
-
-        * ``'posrealint'`` - Select the phase that gives the largest positive integral of the real part.
-        * ``'negrealint'`` - Select the phase that gives the largest negative integral of the real part.
-        * ``'close'`` - Select the phase closest to the average phase of the original data.
-
-        The default behaviour is ``'posrealint'``.
+        Complex-valued 1D or 2D signal.
 
     full_output : boolean, optional
-        If True, the function will return additional output arguments, by default False.
+        If ``True``, return additional output arguments. (default: ``False``)
 
     Returns
     -------
     Vr : ndarray
-        Real part of the phase corrected dataset.
+        Real part of the phase-corrected data.
 
-    Vi : ndarray (if full_output==True)
-        Imaginary part of the phase corrected dataset.
+    Vi : ndarray (only if ``full_output==True``)
+        Imaginary part of the phase-corrected data.
 
-    phase : float scalar or ndarray (if full_output==True)
-        Fitted phase used for correction, in radians.
+    phase : float scalar or ndarray (only if ``full_output==True``)
+        Fitted phase, or list of phases for 2D data, used for correction, in radians.
 
     """
+    
+    if not np.iscomplexobj(V):
+        raise ValueError("Data set must be complex-valued.")
+
+    data1d = V.ndim==1
 
     V_2d = V.copy()
-    if V_2d.ndim == 1:
+    if data1d:
         V_2d = V_2d[:, np.newaxis]
 
-    V_2d = V_2d.T
+    # The follwing determines the phase that minimizes the cost
+    # function = sum of squares of imaginary part of V*exp(1j*phi)
+    # This cost function has the analytical form
+    #
+    #   (A+B) + (B-A)*cos(2*phi) + C*sin(2*phi)
+    #    = offset + amp*cos(2*phi-phi0)
+    #
+    # where
+    #    A = sum_k real(V_k)^2 / 2
+    #    B = sum_k imag(V_k)^2 / 2
+    #    C = sum_k real(V_k)*imag(V_k)
+    #
+    #    offset = A+B
+    #    amp = sqrt((B-A)^2+C^2)
+    #    phi0 = atan2(C, B-A)
+    #
+    # The cost function has two minima:
+    #    phi = phi0/2 + pi/2   and   phi = phi0/2 + 3*pi/2
+    
+    # Calculate phase that minimizes cost function
+    Vr = np.real(V_2d)
+    Vi = np.imag(V_2d)
+    A = np.sum(Vr**2, axis=0)/2
+    B = np.sum(Vi**2, axis=0)/2
+    C = np.sum(Vr*Vi, axis=0)
+    phi0 = np.arctan2(C, B-A)
+    phimin = phi0/2 + np.pi/2  # one of the two minimizers
+    
+    # Apply phase rotation
+    V_2d *= np.exp(1j*phimin)[None,:]
+    
+    # Pick minimizer that yields positive average of real part
+    reAvg = np.average(V_2d, axis=0)
+    idx = reAvg < 0
+    phimin[idx] += np.pi
+    V_2d[:,idx] = -V_2d[:,idx]
 
-    # Calculate 3 points of cost function which should be a smooth, continuous sine wave with a frequency of 2 * phi
-    phis = np.array([0, np.pi / 2, np.pi]) / 2
-    costs = np.imag(V_2d[:, None] * np.exp(1j * phis)[None, :, None])
-    costs = (costs * costs).sum(axis=-1)
-
-    # Calculate sine function fitting 3 points
-    offset = (costs[:, 0] + costs[:, 2]) / 2
-    phase_shift = np.arctan2(costs[:, 0] - offset, costs[:, 1] - offset)
-    amp = np.sqrt((costs[:, 0] - offset) ** 2 + (costs[:, 1] - offset) ** 2)
-
-    # Calculate minima by setting the first derivative 0
-    phis = (3 * np.pi / 2 - phase_shift) / 2
-    phis[amp < 0] -= np.pi / 2
-    phis[phis < 0] += np.pi
-
-    tempspec = V_2d * np.exp(1j * phis)[:, None]
-    if phase == 'posrealint':
-        phis[tempspec.sum(axis=1) < 0] += np.pi
-    elif phase == 'negrealint':
-        phis[tempspec.sum(axis=1) > 0] -= np.pi
-
-    V_2d *= np.exp(1j * phis)[:, None]
-
-    V_2d = np.squeeze(V_2d.T)
-
-    # Output
+    # Assemble output
+    if data1d:
+        V_2d = np.squeeze(V_2d, axis=1)
     Vreal = np.real(V_2d)
     Vimag = np.imag(V_2d)
-
-    # Map phase angle to [-pi,pi) interval
-    phase = phis
-
     if full_output:
-        return Vreal, Vimag, phase
+        return Vreal, Vimag, phimin
     else:
         return Vreal
-
-# ==========================================================================
