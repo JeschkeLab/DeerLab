Fix bug in global weighting system. Closes #300

deerlab/selregparam.py
@@ -183,13 +183,14 @@ def _evalalpha(alpha,y,A,L,solver,selmethod,noiselvl,weights):
 
     # Prepare LSQ components
     AtAreg, Aty = dl.solvers._lsqcomponents(y,A,L,alpha,weights)
+    wA = weights[:,np.newaxis]*A
     # Solve linear LSQ problem
     P = solver(AtAreg,Aty)
 
     # Moore-PeNose pseudoinverse
-    pA = np.linalg.inv(AtAreg)@A.T
+    pA = np.linalg.inv(AtAreg)@wA.T
     # Influence matrix
-    H = A@pA
+    H = wA@pA
 
     # Residual term
     residuals = weights*(A@P - y)

deerlab/solvers.py
@@ -152,8 +152,10 @@ def _lsqcomponents(V, K, L=None, alpha=0, weights=None):
         weights = np.atleast_1d(weights)
         
     # Compute components of the LSQ normal equations
-    KtK = K.T@(weights[:,np.newaxis]*K)
-    KtV = K.T@(weights*V)
+    Kw = weights[:,np.newaxis]*K
+    Vw = weights*V
+    KtK = Kw.T@Kw
+    KtV = Kw.T@Vw
     
     # No regularization term -> done
     if L is None:
@@ -549,7 +551,7 @@ def snlls(y, Amodel, par0=None, lb=None, ub=None, lbl=None, ubl=None, nnlsSolver
         par0 = np.array([])
 
     if regparamrange is None: 
-        regparamrange = [1e-8,1e2]
+        regparamrange = [1e-8,1e3]
 
     # Get info on the problem parameters and non-linear operator
     A0 = Amodel(par0)

test/test_snlls_rlls.py
@@ -303,18 +303,18 @@ def test_global_weights():
     "Check that the global weights properly work when specified"
 
     t = np.linspace(0,5,300)
-    r = np.linspace(2,8,150)
+    r = np.linspace(2,8,200)
     K = dipolarkernel(t,r)
 
-    param1 = [3,0.2]
+    param1 = [3.5,0.3]
     param2 = [5,0.2]
     P1 = dd_gauss(r,*param1)
     P2 = dd_gauss(r,*param2)
     V1 = K@P1 + whitegaussnoise(t,0.01,seed=1)
     V2 = K@P2 + whitegaussnoise(t,0.01,seed=1)
 
-    fit1 = snlls([V1,V2],[K,K],lbl=np.zeros_like(r),weights=[1,1e-10])
-    fit2 = snlls([V1,V2],[K,K],lbl=np.zeros_like(r),weights=[1e-10,1])
+    fit1 = snlls([V1,V2],[K,K],lbl=np.zeros_like(r),weights=[1,0])
+    fit2 = snlls([V1,V2],[K,K],lbl=np.zeros_like(r),weights=[0,1])
 
     assert ovl(P1,fit1.param) > 0.95 and ovl(P2,fit2.param) > 0.95
 # ======================================================================