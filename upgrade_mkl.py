d4c37a5caeadd93d38a616d3e0c4a677478461e3
Luis Fabregas <luis.fabregas@phys.chem.ethz.ch>
2022-06-08 13:17:40 +0200

    fix MKL upgrade script

--- upgrade_mkl.py
+++ upgrade_mkl.py
@@ -1,9 +1,6 @@
 from sys import executable
 import platform
 import subprocess
-import os
-
-MKL_LINKED = os.environ.get('MKL_LINKED', False)
 
 # Custom OS-specific installation script
 def install_MKL_dependencies():
@@ -12,18 +9,17 @@ def install_MKL_dependencies():
     if platform.system() != 'Windows':
         raise SystemError('MKL dependencies are only available on Windows systems.')
 
+    subprocess.run([executable,'-m','pip','uninstall','numpy'],check=False)
+    subprocess.run([executable,'-m','pip','uninstall','scipy'],check=False)
+    subprocess.run([executable,'-m','pip','uninstall','cvxopt'],check=False)
+
     # At the moment the dependencies must be handled manually this way
     subprocess.run([executable,'-m','pip','install','git+https://github.com/luisfabib/pipwin'],check=False)
 
-    try:
-        # Install Numpy,SciPy, CVXopt linked to MKL from Gohlken's repository
-        subprocess.run([executable,'-m','pipwin','install','numpy','--filter=mkl'],check=False)
-        subprocess.run([executable,'-m','pipwin','install','scipy'],check=False)
-        subprocess.run([executable,'-m','pipwin','install','cvxopt'],check=False)
-    except: 
-        subprocess.run([executable,'-m','pipwin','install','numpy','--filter=mkl','--user'],check=False)
-        subprocess.run([executable,'-m','pipwin','install','scipy','--user'],check=False)
-        subprocess.run([executable,'-m','pipwin','install','cvxopt','--user'],check=False)
+    # Install Numpy,SciPy, CVXopt linked to MKL from Gohlken's repository
+    subprocess.run([executable,'-m','pipwin','install','numpy','--filter=mkl','--user'],check=False)
+    subprocess.run([executable,'-m','pipwin','install','scipy','--user'],check=False)
+    subprocess.run([executable,'-m','pipwin','install','cvxopt','--user'],check=False)
 #-----------------------------------------------------------------------    
 
 
