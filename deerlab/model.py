013b383d29e7ded4ffd6e771b2c601b52b23a4b2
Luis Fabregas <48292540+luisfabib@users.noreply.github.com>
2022-07-30 18:42:55 +0200

    Minor edits to files edited in previous PRs (#356)
    * fix minor logic error
    
    * minor documentation update
    
    * fix minor bug

--- deerlab/model.py
+++ deerlab/model.py
@@ -939,15 +939,17 @@ def _print_fitresults(fitresult,model):
     
     hasregularization = fitresult.regparam!=0
     haspenalties = fitresult.penweights
-    if hasregularization and haspenalties:
+    if hasregularization or haspenalties:
         string += 'Model hyperparameters: \n'
         tags = []
         values = []
         alignment = [] 
         if hasregularization:
             alignment.append('^')
-            tags.append('Regularization parameter')        
-            values.append(fitresult.regparam) 
+            tags.append('Regularization parameter')      
+            regparam = fitresult.regparam
+            if regparam is None: regparam = 0  
+            values.append(regparam) 
         if haspenalties:
             for n,penweight in enumerate(fitresult.penweights):
                 alignment.append('^')
