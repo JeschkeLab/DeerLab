6a0b992516dd53b6a8af79300f2520b3d5db14aa
Hugo Karas <hkaras@ethz.ch>
2024-07-02 07:57:54 +0200

    Numpy2 support (#479)
    * Update for 3.12
    
    * Updated workflows and changelog
    
    * Upload to 3.12
    
    * Numpy 2.0 compatibility updates
    
    * Update Changelog

--- deerlab/dd_models.py
+++ deerlab/dd_models.py
@@ -110,7 +110,7 @@ def _multigaussfun(r,r0,sig):
     P = np.sqrt(1/(2*np.pi))*1/sig*np.exp(-0.5*((r.T-r0)/sig)**2)
     if not np.all(P==0):
         # Normalization
-        P = np.squeeze(P)/np.sum([np.trapz(c,r) for c in P.T])
+        P = np.squeeze(P)/np.sum([np.trapezoid(c,r) for c in P.T])
     else: 
         P = np.squeeze(P)
     return P
